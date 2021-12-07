e712d7c4ffe51daf28bddde17642f60f77244b17
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-06 23:16:55 -0300

    created GET request function

--- frontend/src/app/search/services/search.service.ts
+++ frontend/src/app/search/services/search.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  readonly apiUrl = "/api";
+
+  constructor(private http:HttpClient) { }
+
+  getCNPJ(cnpj: string): Observable<any>{
+    return this.http.get<any>(this.apiUrl+cnpj);
+  }
+
 }
