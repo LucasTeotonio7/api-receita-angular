a0ae604c5a1c09908c3923e96f2ef1a0adddca42
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-07 23:51:56 -0300

    applied cnpj mask and added modal

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './services/search.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CnpjDetailComponent } from '../cnpj-detail/cnpj-detail.component';
 
 @Component({
   selector: 'app-search',
@@ -10,13 +12,27 @@ export class SearchComponent implements OnInit {
 
   //TODO: remove test cnpj
   CNPJ:string = '06990590000123';
+  // object: string = '';
 
-  constructor(private SearchService:SearchService ) { }
+  constructor(private SearchService:SearchService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.SearchService.getCNPJ(this.CNPJ).subscribe(data=>{
       console.log(data)
     })
+  }
+
+  openDialog(): void {
+    const weekRef = this.dialog.open(CnpjDetailComponent, {
+      width: '500px',
+      data: this.CNPJ
+    });
+
+    weekRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.object = result;
+      this.ngOnInit()
+    });
   }
 
 }
