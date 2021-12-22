9111d51748c86851ddcb5bbfe08fc48e25248d02
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-21 22:34:57 -0300

    called service in search component

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+
 import { CnpjDetailComponent } from '../cnpj-detail/cnpj-detail.component';
+import { SearchService } from '../search/services/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
+
 })
 export class SearchComponent implements OnInit {
 
-  CNPJ:string = '';
+  CNPJ: string = '';
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private SearchService:SearchService) { }
 
   ngOnInit(): void {}
 
   openDialog(): void {
-    this.dialog.open(CnpjDetailComponent, {
-      width: '60%',
-      maxHeight: '80%',
-      data: this.CNPJ
-    });
+    this.SearchService.getCNPJ(this.CNPJ).subscribe(data=>{
+
+      if(data.status == 'OK'){
+        this.dialog.open(CnpjDetailComponent, {
+          width: '60%',
+          maxHeight: '95%',
+          data: data
+        });
+      } else{
+        this.SearchService.errorMessage(data.message);
+      }
+    })
   }
 
 }
