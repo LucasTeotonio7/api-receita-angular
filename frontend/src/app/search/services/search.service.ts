63f464c4ff6b5fd19f9ee7f5f22d25203b9bfa88
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-21 22:37:41 -0300

    created error function

--- frontend/src/app/search/services/search.service.ts
+++ frontend/src/app/search/services/search.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from'@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,19 @@ export class SearchService {
 
   readonly apiUrl = "/api";
 
-  constructor(private http:HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http:HttpClient) { }
 
   getCNPJ(cnpj: string): Observable<any>{
     return this.http.get<any>(this.apiUrl+cnpj);
+  }
+
+  errorMessage(msg: string):void{
+    this.snackBar.open(msg,'X',{
+      duration: Infinity,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: 'msg-error',
+    })
   }
 
 }
