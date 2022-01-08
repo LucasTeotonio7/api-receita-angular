839cca78099f726b9227a9a4fe19bfd591c64e3e
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-01-07 23:11:08 -0300

    feat: added close button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchModule } from './search/search.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CnpjDetailComponent } from './cnpj-detail/cnpj-detail.component';
 
 @NgModule({
@@ -17,6 +19,8 @@ import { CnpjDetailComponent } from './cnpj-detail/cnpj-detail.component';
     AppRoutingModule,
     SearchModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/cnpj-detail/cnpj-detail.component.ts
+++ frontend/src/app/cnpj-detail/cnpj-detail.component.ts
@@ -19,4 +19,8 @@ export class CnpjDetailComponent implements OnInit {
     this.cnpj = this.data;
   }
 
+  closeModal(){
+    this.weekRef.close();
+  }
+
 }
