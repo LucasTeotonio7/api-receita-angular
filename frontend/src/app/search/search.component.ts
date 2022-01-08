3f054cc5deeb4a6785aa079168d4bcc9eda49bfb
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-01-07 23:10:06 -0300

    fix:fixed error messages

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -20,16 +20,26 @@ export class SearchComponent implements OnInit {
 
   openDialog(): void {
     this.SearchService.getCNPJ(this.CNPJ).subscribe(data=>{
-
-      if(data.status == 'OK'){
         this.dialog.open(CnpjDetailComponent, {
           width: '60%',
           maxHeight: '95%',
           data: data
         });
-      } else{
-        this.SearchService.errorMessage(data.message);
+
+    }, error=>{
+
+      if(error.status === 400){
+        this.SearchService.errorMessage("O CNPJ informado é inválido!");
       }
+
+      else if(error.status === 429){
+        this.SearchService.errorMessage("Você só pode fazer 3 requisições por minuto! Aguarde um momento... ");
+      }
+
+      else{
+        this.SearchService.errorMessage("Erro Interno");
+      }
+
     })
   }
 
